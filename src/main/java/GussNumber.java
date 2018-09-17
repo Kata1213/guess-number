a8be98799a16c276a26cb546ff8fd0de9c4d6b4c
Jingyue Li <jingyli@thoughtworks.com>
2018-09-17 14:15:50 +0800

    finish second function

--- src/main/java/GussNumber.java
+++ src/main/java/GussNumber.java
@@ -1,12 +1,40 @@
+import com.google.common.base.Strings;
+import com.google.common.io.LineReader;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.Scanner;
 import java.util.stream.Collectors;
 
 public class GussNumber {
 
+    public static void main(String []args) throws IOException {
+        LineReader lineReader = new LineReader(new InputStreamReader(System.in));
+        String line;
+        while (!Strings.isNullOrEmpty((line=lineReader.readLine()))){
+            System.out.println(new GussNumber().process(line));
+            System.out.println(new GussNumber().caculateOdd(line));
+        }
+    }
+
     public  String process(String input){
-        return Arrays.stream(input.split(" "))
-                .filter(x->Integer.parseInt(x)%2!=0)
+        return  Arrays.stream(input.split(" "))
+                .map(Integer::parseInt)
+                .filter(x->x%2!=0)
+                .map(x->(x*2+1))
+                .map(String::valueOf)
                 .collect(Collectors.joining(" "));
+    }
+
+
+    public String caculateOdd(String s) {
+        return Arrays.stream(s.split(" "))
+                .map(Integer::parseInt)
+                .filter(x->x%2==0)
+                .map(x->(x/2))
+                .map(String::valueOf)
+                .collect(Collectors.joining(" "));
+
     }
 }
